a8451dfaf7c781629895176da09784fe75381318
Davlik <slon.zelenyj@gmail.com>
2023-05-04 15:04:03 +0600

    feat: task 4 done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,17 @@
+const fs = require('fs').promises;
+const path = require('path');
 
+async function copyDir() {
+  const dir = path.join(__dirname, 'files');
+  const newDir = path.join(__dirname, 'files-copy');
+  await fs.rm(newDir, { recursive: true, force: true });
+  await fs.mkdir(newDir);
+  
+  const files = await fs.readdir(dir);
+  for (const file of files) {
+    const srcPath = path.join(dir, file);
+    const destPath = path.join(newDir, file);
+    await fs.copyFile(srcPath, destPath);
+  }
+}
+copyDir();
