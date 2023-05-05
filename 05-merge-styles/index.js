5b77d37f0c7bb46c7f81eba73e1ded1fce338035
Davlik <slon.zelenyj@gmail.com>
2023-05-05 13:15:07 +0600

    feat: task 5 done

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+async function createBundle() {
+  const stylesDir = path.join(__dirname, 'styles');
+  const stylesArr = [];
+  const files = await fs.readdir(stylesDir);
+  for (const file of files) {
+    const filePath = path.join(stylesDir, file);
+    const stat = await fs.stat(filePath);
+    if (stat.isFile() && path.extname(filePath) === '.css') {
+      const cssData = await fs.readFile(filePath, 'utf-8');
+      stylesArr.push(cssData);
+    }
+  }
+  const bundlePath = path.join(__dirname, 'project-dist', 'bundle.css');
+  await fs.rm(bundlePath, { recoursive: true, force: true });
+  await fs.writeFile(bundlePath, stylesArr.join('\n'));
+}
+createBundle();
